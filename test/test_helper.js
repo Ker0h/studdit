e09cdbfd3bf3a69320a6a6909a7f79c641bbf638
Stijn van Veen <stijnboz@live.nl>
2018-11-26 12:11:55 +0100

    fix test helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
+before(() => {
 mongoose.connect('mongodb://localhost/studdit')
-
 mongoose.connection
     .once('open', () => console.log('Connected to studdit...'))
     .on('error', (error) => {
         console.warn('Warning:', error)
     })
+})
 
 beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
